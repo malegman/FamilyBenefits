0ad1de0baabe8af35643eec120ff453c17b20de8
malegman <smegovic@gmail.com>
2022-01-11 18:26:08 +0500

    Удаление лишних импортов

--- src/main/java/com/example/familybenefits/FamilyBenefitsApplication.java
+++ src/main/java/com/example/familybenefits/FamilyBenefitsApplication.java
@@ -2,11 +2,6 @@ package com.example.familybenefits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
-import java.net.http.HttpClient;
 
 @SpringBootApplication
 public class FamilyBenefitsApplication {
