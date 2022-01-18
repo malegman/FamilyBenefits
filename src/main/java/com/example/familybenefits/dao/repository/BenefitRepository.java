cd3d4f77290e4ba68806e02d861a54bcc02b391f
malegman <smegovic@gmail.com>
2022-01-18 23:34:51 +0500

    Изменение методов по запросам sql

--- src/main/java/com/example/familybenefits/dao/repository/BenefitRepository.java
+++ src/main/java/com/example/familybenefits/dao/repository/BenefitRepository.java
@@ -1,9 +1,8 @@
 package com.example.familybenefits.dao.repository;
 
 import com.example.familybenefits.dao.entity.BenefitEntity;
-import com.example.familybenefits.dao.entity.CityEntity;
-import com.example.familybenefits.dao.entity.InstitutionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigInteger;
 import java.util.Set;
@@ -14,16 +13,26 @@ import java.util.Set;
 public interface BenefitRepository extends JpaRepository<BenefitEntity, BigInteger> {
 
   /**
-   * Находит пособия, которые есть в указанных городах
-   * @param cityEntitySet множество городов
+   * Находит пособия, которые есть в указанном городе
+   * @param idCity ID города
    * @return множество пособий
    */
-  Set<BenefitEntity> findAllByCityEntitySet(Set<CityEntity> cityEntitySet);
+  @Query(nativeQuery = true,
+      value = "SELECT *" +
+          "FROM familybenefit.benefits_cities INNER JOIN familybenefit.benefit ON " +
+          "familybenefit.benefits_cities.id_benefit = familybenefit.benefit.id " +
+          "WHERE familybenefit.benefits_cities.id_city = ?1;")
+  Set<BenefitEntity> findAllWhereCityIdEquals(BigInteger idCity);
 
   /**
-   * Находит пособия, которые есть в указанных учреждениях
-   * @param institutionEntitySet множество учреждений
+   * Находит пособия, которые есть в указанном учреждении
+   * @param idInstitution ID учреждения
    * @return множество пособий
    */
-  Set<BenefitEntity> findAllByInstitutionEntitySet(Set<InstitutionEntity> institutionEntitySet);
+  @Query(nativeQuery = true,
+      value = "SELECT *" +
+          "FROM familybenefit.benefits_institutions INNER JOIN familybenefit.benefit ON " +
+          "familybenefit.benefits_institutions.id_benefit = familybenefit.benefit.id " +
+          "WHERE familybenefit.benefits_institutions.id_institution = ?1;")
+  Set<BenefitEntity> findAllWhereInstitutionIdEquals(BigInteger idInstitution);
 }
