4555c2389a7e49220054d93a832f9d39a4d1e9e3
malegman <smegovic@gmail.com>
2022-02-01 01:16:36 +0500

    Изменение метода фильтрации и извлечения токена из запроса

--- src/main/java/com/example/familybenefits/security/service/implementation/JwtFilter.java
+++ src/main/java/com/example/familybenefits/security/service/implementation/JwtFilter.java
@@ -1,12 +1,15 @@
-package com.example.familybenefits.security.service;
+package com.example.familybenefits.security.service.implementation;
 
+import com.example.familybenefits.exception.InvalidEmailException;
+import com.example.familybenefits.security.service.s_interface.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -17,17 +20,16 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.Optional;
 
 /**
- * Фильтр для фильрации клиентских запросов, с использованием JWT
+ * Фильтр для фильтрации клиентских запросов, с использованием JWT
  */
 @Slf4j
 @Service
 public class JwtFilter extends GenericFilterBean {
 
   /**
-   * Название заголовка запроса, для извлечния токена доступа
+   * Название заголовка запроса, для извлечения токена доступа
    */
   public static final String AUTHORIZATION = "Authorization";
 
@@ -63,55 +65,42 @@ public class JwtFilter extends GenericFilterBean {
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
-    String token;
-    String email;
+    try {
+      // Извлечение токена из запроса, и получение пользователя на основе данных токена (email пользователя)
+      String token = getTokenFromRequest((HttpServletRequest) servletRequest);
+      Jws<Claims> jwt = jwtService.convertToJwt(token);
+      String email = jwtService.getEmailFromToken(jwt);
+      UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
-    Optional<String> optToken = getTokenFromRequest((HttpServletRequest) servletRequest);
+      // Получение данных авторизации пользователя и установка их контекст безопасности
+      UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(auth);
 
-    if (optToken.isEmpty()) {
-      log.warn("Couldn't get token from request: [{}]", servletRequest);
-    } else {
-      token = optToken.get();
-      if (jwtService.validateToken(token)) {
-        Optional<String> optEmail = jwtService.getEmailFromToken(token);
-        if (optEmail.isEmpty()) {
-          log.warn("Couldn't get email from token: [{}]", token);
-        } else {
-          email = optEmail.get();
-          UserDetails userDetails = null;
-          try {
-            userDetails = userDetailsService.loadUserByUsername(email);
-          } catch (UsernameNotFoundException e) {
-            log.error("UserDetails with email [{}] not found", email);
-          }
-          if (userDetails != null) {
-            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(auth);
-          }
-        }
-      }
+    } catch (RuntimeException | InvalidEmailException e) {
+      // Ошибки извлечения токена из заброса, преобразования токена в jwt,
+      // извлечения email из jwt, получения пользователя по email
+      log.error(e.getMessage());
+
+    } finally {
+      // Фильтрование запроса с использованием контекста безопасности
+      filterChain.doFilter(servletRequest, servletResponse);
     }
-
-    filterChain.doFilter(servletRequest, servletResponse);
   }
 
   /**
    * Извлекает токен из запроса клиента
    * @param request запрос клиента
-   * @return токен пользователя, jwt. null, если токен не обнаружен или request == null
+   * @return токен доступа пользователя.
+   * @throws IllegalArgumentException если не удалось извлечь токен
    */
-  private Optional<String> getTokenFromRequest(HttpServletRequest request) {
-
-    if (request == null) {
-      return Optional.empty();
-    }
+  private String getTokenFromRequest(HttpServletRequest request) throws IllegalArgumentException {
 
     String bearer = request.getHeader(AUTHORIZATION);
-
     if (StringUtils.hasText(bearer) && bearer.startsWith("Bearer ")) {
-      return Optional.of(bearer.substring(7));
-    }
+      return bearer.substring(7);
 
-    return Optional.empty();
+    } else {
+      throw new IllegalArgumentException("Can't get token from request: " + request);
+    }
   }
 }
