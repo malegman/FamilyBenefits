f1f42f492f98fa1ce84ef8f8bcd95e14bd7b839f
malegman <smegovic@gmail.com>
2022-02-02 21:52:13 +0500

    Изменение типа поля даты рождения

--- src/main/resources/db/migration/V20220123152215__create_child.sql
+++ src/main/resources/db/migration/V20220123152215__create_child.sql
@@ -2,7 +2,7 @@ CREATE TABLE family_benefit.child (
 
   -- Диапазон ID [1 000 000 000; 9 999 999 999]
   "id" NUMERIC NOT NULL DEFAULT (10000000000 - 1000000000) * random() + 1000000000,
-  "date_birth" TIMESTAMP NOT NULL,
+  "date_birth" DATE NOT NULL,
 
   CONSTRAINT child_pk PRIMARY KEY ("id"),
   CONSTRAINT child_uniq_birth UNIQUE ("date_birth")
