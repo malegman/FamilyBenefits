5d222ea4b5ffbea6aeb9b2c6064e9316befe4d16
malegman <smegovic@gmail.com>
2022-02-13 22:15:47 +0500

    Переименование методов, добавление метода фильтрации, удаление метода на дополнительные данные

--- src/main/java/com/example/familybenefits/service/s_interface/CityService.java
+++ src/main/java/com/example/familybenefits/service/s_interface/CityService.java
@@ -1,9 +1,8 @@
 package com.example.familybenefits.service.s_interface;
 
-import com.example.familybenefits.api_model.city.CityAdd;
+import com.example.familybenefits.api_model.city.CitySave;
 import com.example.familybenefits.api_model.city.CityInfo;
-import com.example.familybenefits.api_model.city.CityInitData;
-import com.example.familybenefits.api_model.city.CityUpdate;
+import com.example.familybenefits.api_model.common.ObjectShortInfo;
 import com.example.familybenefits.exception.AlreadyExistsException;
 import com.example.familybenefits.exception.NotFoundException;
 
@@ -15,26 +14,22 @@ import java.util.Set;
 public interface CityService {
 
   /**
-   * Добавляет город по запросу на добавление
-   * @param cityAdd объект запроса на добавление города
+   * Возвращает множество городов, в которых есть учреждения и пособия.
+   * Фильтр по названию города и пособию.
+   * В качестве параметра может быть указан null, если данный параметр не участвует в фильтрации
+   * @param nameCity Название города
+   * @param idBenefit ID пособия
+   * @return множество кратких информаций о городах
+   */
+  Set<ObjectShortInfo> readAllFilter(String nameCity, String idBenefit);
+
+  /**
+   * Создает город по запросу на сохранение
+   * @param citySave объект запроса на сохранение города
    * @throws AlreadyExistsException если город с указанным названием уже существует
    * @throws NotFoundException если пособие города с указанным ID не найдено
    */
-  void add(CityAdd cityAdd) throws AlreadyExistsException, NotFoundException;
-
-  /**
-   * Обновляет город по запросу на обновление
-   * @param cityUpdate объект запроса на обновление города
-   * @throws NotFoundException если город с указанными данными не найден
-   */
-  void update(CityUpdate cityUpdate) throws NotFoundException;
-
-  /**
-   * Удаляет город по его ID
-   * @param idCity ID города
-   * @throws NotFoundException если город с указанным ID не найден
-   */
-  void delete(String idCity) throws NotFoundException;
+  void create(CitySave citySave) throws AlreadyExistsException, NotFoundException;
 
   /**
    * Возвращает информацию о городе по его ID
@@ -45,21 +40,23 @@ public interface CityService {
   CityInfo read(String idCity) throws NotFoundException;
 
   /**
-   * Возвращает множество городов, в которых есть учреждения и пособия
-   * @return множество информаций о городах
+   * Обновляет город по запросу на сохранение
+   * @param idCity ID города
+   * @param citySave объект запроса на сохранение города
+   * @throws NotFoundException если город с указанным ID не найден
    */
-  Set<CityInfo> getAll();
+  void update(String idCity, CitySave citySave) throws NotFoundException;
+
+  /**
+   * Удаляет город по его ID
+   * @param idCity ID города
+   * @throws NotFoundException если город с указанным ID не найден
+   */
+  void delete(String idCity) throws NotFoundException;
 
   /**
    * Возвращает множество городов, в которых нет учреждений или пособий
-   * @return множество информаций о городах
+   * @return множество кратких информаций о городах
    */
-  Set<CityInfo> getAllPartial();
-
-  /**
-   * Возвращает дополнительные данные для города.
-   * Данные содержат в себе множества кратких информаций о пособиях
-   * @return дополнительные данные для города
-   */
-  CityInitData getInitData();
+  Set<ObjectShortInfo> readAllPartial();
 }
