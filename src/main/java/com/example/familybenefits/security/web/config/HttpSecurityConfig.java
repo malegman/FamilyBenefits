d8421e01365c5a55fa1e12505d6f392b722ddf12
malegman <smegovic@gmail.com>
2022-02-15 19:49:06 +0500

    Удаление лишних импортов

--- src/main/java/com/example/familybenefits/security/web/config/HttpSecurityConfig.java
+++ src/main/java/com/example/familybenefits/security/web/config/HttpSecurityConfig.java
@@ -1,9 +1,6 @@
 package com.example.familybenefits.security.web.config;
 
-import com.example.familybenefits.security.web.filter.UserAuthenticationFilterFB;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
