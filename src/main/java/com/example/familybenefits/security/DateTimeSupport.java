3e8ca7575e3b669f877b14dd619f5a4a1751530b
malegman <smegovic@gmail.com>
2022-04-03 22:49:13 +0500

    Преобразование сервиса в класс со статическими методами, добавление проверки времени

---
+++ src/main/java/com/example/familybenefits/security/DateTimeSupport.java
@@ -0,0 +1,118 @@
+package com.example.familybenefits.security;
+
+import com.example.familybenefits.exceptions.DateFormatException;
+import com.example.familybenefits.exceptions.DateTimeException;
+import com.example.familybenefits.resources.R;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalAccessor;
+import java.util.*;
+
+/**
+ * Предоставляет статические методы для упрощенной работы с датой и временем
+ */
+public class DateTimeSupport {
+
+  /**
+   * Возвращает дату истечения срока жизни. Дата формируется из текущего времени и прибавленных секунд
+   * @param expireSec число секунд, по окончанию которых истечет срок
+   * @return дата истечения срока жизни
+   */
+  public static LocalDateTime getExpiration(long expireSec) {
+
+    return LocalDateTime.now().plusSeconds(expireSec);
+  }
+
+  /**
+   * Преобразует строку формата "dd.mm.yyyy" в дату
+   * @param userBirth дата в строковом виде
+   * @return преобразованная строка в формат даты
+   * @throws DateFormatException если полученная строка не соответствует формату "dd.mm.yyyy"
+   */
+  public static LocalDate strToDate(String userBirth) throws DateFormatException {
+
+    try {
+      return LocalDate.from((TemporalAccessor) R.SIMPLE_DATE_FORMAT.parse(userBirth));
+    } catch (ParseException e) {
+      throw new DateFormatException(String.format(
+          "The string \"%s\" doesn't match the date format \"dd.mm.yyyy\"", userBirth));
+    }
+  }
+
+  /**
+   * Преобразует строки формата "dd.mm.yyyy" в даты
+   *
+   * @param userBirthList список дат в строковом виде
+   * @return преобразованная строка в формат даты
+   * @throws DateFormatException если одна из полученных строк не соответствует формату "dd.mm.yyyy"
+   */
+  public static List<LocalDate> strToDate(List<String> userBirthList) throws DateFormatException {
+
+    List<LocalDate> localDateList = new ArrayList<>(userBirthList.size());
+    for (String userBirth : userBirthList) {
+      localDateList.add(strToDate(userBirth));
+    }
+
+    return localDateList;
+  }
+
+  /**
+   * Проверяет дату на предшествие текущей дате
+   * @param dateCheck проверяемая дата
+   * @throws DateTimeException если проверяемая дата позже текущей даты
+   */
+  public static void checkDateBeforeNow(LocalDate dateCheck) throws DateTimeException {
+
+    LocalDate dateCurrent = LocalDate.now();
+    if (dateCheck.isAfter(dateCurrent)) {
+      throw new DateTimeException(String.format(
+          "The date \"%s\" is after current date \"%s\"", dateCheck, dateCurrent));
+    }
+  }
+
+  /**
+   * Проверяет множество дат на предшествие текущей дате
+   * @param dateList множество проверяемых дат
+   * @throws DateTimeException если проверяемая дата позже текущей даты
+   */
+  public static void checkDateBeforeNow(List<LocalDate> dateList) throws DateTimeException {
+
+    for (LocalDate date : dateList) {
+      checkDateBeforeNow(date);
+    }
+  }
+
+  /**
+   * Проверяет текущее время на предшествие проверяемому времени
+   * @param dateTimeCheck проверяемое время
+   * @throws DateTimeException если текущее время позже проверяемого
+   */
+  public static void checkDateTimeAfterNow(LocalDateTime dateTimeCheck) throws DateTimeException {
+
+    LocalDateTime dateTimeCurrent = LocalDateTime.now();
+    if (dateTimeCurrent.isAfter(dateTimeCheck)) {
+      throw new DateTimeException(String.format(
+          "Current time \"%s\" is after check time \"%s\"", dateTimeCurrent, dateTimeCheck));
+    }
+  }
+
+  /**
+   * Проверяет, был ли день рождения после проверяемой даты
+   * @param dateBirth дата рождения
+   * @param dateCheck проверяемая дата
+   * @throws DateTimeException если день рождения был после проверяемой даты
+   */
+  public static void checkBirthdayBefore(LocalDate dateBirth, LocalDate dateCheck) throws DateTimeException {
+
+    if (dateBirth.plusYears(LocalDate.now().getYear() - dateBirth.getYear()).isAfter(dateCheck)) {
+      throw new DateTimeException(String.format(
+          "The day of birthday of date \"%s\" was after check date \"%s\"", dateBirth, dateCheck));
+    }
+  }
+}
+
