d8da1470679a839e8db3c5d70f5927d79badf8f8
malegman <smegovic@gmail.com>
2022-04-03 22:55:08 +0500

    Контейнеры данных безопасности

---
+++ src/main/java/com/example/familybenefits/part_auth/models/AuthData.java
@@ -0,0 +1,24 @@
+package com.example.familybenefits.part_auth.models;
+
+import com.example.familybenefits.part_auth.services.interfaces.TokenCodeService;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Контейнер, содержащий данные токены доступа (jwt) и восстановления. Создается сервисом {@link TokenCodeService}
+ */
+@Builder
+@Getter
+public class AuthData {
+
+  /**
+   * Данные токена доступа jwt
+   */
+  private JwtData jwtData;
+
+  /**
+   * Токен восстановления
+   */
+  private String refreshToken;
+}
+
---
+++ src/main/java/com/example/familybenefits/part_auth/models/JwtData.java
@@ -0,0 +1,23 @@
+package com.example.familybenefits.part_auth.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Контейнер, содержащий токен доступа формата jwt и его полезную нагрузку, данные пользователя {@link JwtUserData}.
+ */
+@Builder
+@Getter
+public class JwtData {
+
+  /**
+   * Токен доступа в формате jwt
+   */
+  private String tokenJwt;
+
+  /**
+   * Данные пользователя
+   */
+  private JwtUserData userData;
+}
+
