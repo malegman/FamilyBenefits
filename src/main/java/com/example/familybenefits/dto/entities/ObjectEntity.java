07d40e900da54c2365c6f096e786f58c7475fb5d
malegman <smegovic@gmail.com>
2022-04-03 22:43:05 +0500

    Перенос функций эквивалентности и хэшкода в наследующие классы

--- src/main/java/com/example/familybenefits/dto/entities/ObjectEntity.java
+++ src/main/java/com/example/familybenefits/dto/entities/ObjectEntity.java
@@ -4,10 +4,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.Hibernate;
-import org.springframework.lang.Nullable;
-
-import java.util.Objects;
 
 /**
  * Базовый класс для моделей таблиц
