aa998ec60872f6ad6b9bee5c5edab2935b797870
malegman <smegovic@gmail.com>
2022-04-03 22:37:13 +0500

    Преобразование токена доступа в токен восстановления

---
+++ src/main/java/com/example/familybenefits/dto/repositories/RefreshTokenRepository.java
@@ -0,0 +1,34 @@
+package com.example.familybenefits.dto.repositories;
+
+import com.example.familybenefits.dto.entities.RefreshTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий, работающий с моделью таблицы "refresh_token"
+ */
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, String> {
+
+  /**
+   * Проверяет наличие модели токена восстановления по его значению
+   * @param refreshToken значение токена восстановления
+   * @return true, если модель с указанным токеном существует
+   */
+  boolean existsByToken(String refreshToken);
+
+  /**
+   * Возвращает модель токена восстановления
+   * @param refreshToken токен восстановления
+   * @return модель токена восстановления, или {@code empty}, если токен не найден
+   */
+  Optional<RefreshTokenEntity> findByToken(String refreshToken);
+
+  /**
+   * Удаляет модель токена восстановления по значению токена
+   * @param refreshToken значение токена восстановления
+   */
+  @Modifying
+  void deleteByToken(String refreshToken);
+}
