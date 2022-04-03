09c97898ef8654fe8375ef6130d2d8339a914aff
malegman <smegovic@gmail.com>
2022-04-03 22:45:49 +0500

    Создание исключения для проверок строковых значений

---
+++ src/main/java/com/example/familybenefits/exceptions/InvalidStringException.java
@@ -0,0 +1,20 @@
+package com.example.familybenefits.exceptions;
+
+/**
+ * Исключение, связанное со строковым параметром из объекта запроса
+ */
+public class InvalidStringException extends Exception {
+
+  /**
+   * Код варианта ошибки в api
+   */
+  public static final int API_VARIANT_CODE = 5;
+
+  /**
+   * Конструктор, создает исключение с описанием исключения
+   * @param message описание исключения
+   */
+  public InvalidStringException(String message) {
+    super(message);
+  }
+}
